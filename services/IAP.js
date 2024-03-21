e10331022b0ffd20581839e854c6df1883368bb9
albretsen <bjellanda@gmail.com>
2024-03-21 19:10:03 +0100

    Added iOS IAP key

--- services/IAP.js
+++ services/IAP.js
@@ -2,22 +2,30 @@ import { Platform } from 'react-native';
 import Purchases, { LOG_LEVEL } from 'react-native-purchases';
 
 const APIKeys = {
-    google: "goog_YJGsWAxeZlSBBpSFrexgBgZpgSW",
+    google: "goog_StXAnSKrfsHDKiLFViYdoaKkCGC",
+    apple: "appl_KnTLeYGyzTNCwpuSgTMFXPvfINe",
 };
 
 export const initialize = async () => {
     try {
+        Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
         console.log("Initializing IAP");
 
         if (Platform.OS == "android") {
             await Purchases.configure({ apiKey: APIKeys.google });
+            console.log("PASSED CONFIGURE");
+        } else if (Platform.OS == "ios") {
+            await Purchases.configure({ apiKey: APIKeys.apple });
         } else {
             return;
-        }// 
+        }
 
-        Purchases.setLogLevel(LOG_LEVEL.DEBUG);
+        const customerInfo = await Purchases.getCustomerInfo();
+        console.log("CUSTOMER INFO: ", customerInfo);
 
+        console.log("REACHED OFFERINGS");
         const offerings = await Purchases.getOfferings();
+        console.log("PASSED OFFERINGS");
 
         console.log("Offerings: ", offerings);
     } catch {
