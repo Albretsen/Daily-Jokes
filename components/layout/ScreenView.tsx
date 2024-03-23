6d66ec90ac1cea3ba6bf01d0cda78c76c3664d1e
Hallvard Hetlelid <epichallvard@hotmail.com>
2024-03-23 14:57:34 +0100

    Fixed layout issues, StatusBar only works on Android

--- components/layout/ScreenView.tsx
+++ components/layout/ScreenView.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ReactNode } from 'react';
-import { View, Dimensions, KeyboardAvoidingView, ScrollView, StyleSheet, StyleProp, TextStyle, StatusBar } from 'react-native';
+import { View, Dimensions, KeyboardAvoidingView, SafeAreaView, ScrollView, StyleSheet, StyleProp, TextStyle, StatusBar } from 'react-native';
 import GradientBackground from './GradientBackground';
 import TabBar from '../TabBar/TabBar';
 
@@ -13,8 +13,9 @@ interface ScreenViewProps {
 
 export const HEADER_HEIGHT = 64 + (StatusBar.currentHeight ? StatusBar.currentHeight : 0);
 export const TAB_BAR_HEIGHT = 100;
+export const AD_HEIGHT = 61; // TODO: This would ideally be imported from the ad component
 
-export const SCREEN_HEIGHT = Dimensions.get("screen").height - (HEADER_HEIGHT + TAB_BAR_HEIGHT + 20);
+export const SCREEN_HEIGHT = Dimensions.get("screen").height - (HEADER_HEIGHT + TAB_BAR_HEIGHT + AD_HEIGHT + 20);
 
 export default function ScreenView(props: ScreenViewProps) {
     const { children, style, scrollView = true, hideTabBar = false } = props;
@@ -22,17 +23,19 @@ export default function ScreenView(props: ScreenViewProps) {
     return (
         <View style={styles.container}>
             <GradientBackground />
-            <KeyboardAvoidingView style={{ flex: 1 }}>
-                {scrollView ? (
-                    <ScrollView contentContainerStyle={[styles.screenContainer, style]}>
-                        {children}
-                    </ScrollView>
-                ) : (
-                    <View style={[styles.screenContainer, style]}>
-                        {children}
-                    </View>
-                )}
-            </KeyboardAvoidingView>
+            <SafeAreaView style={{ flex: 1 }}>
+                <KeyboardAvoidingView style={{ flex: 1 }}>
+                    {scrollView ? (
+                        <ScrollView contentContainerStyle={[styles.screenContainer, style]}>
+                            {children}
+                        </ScrollView>
+                    ) : (
+                        <View style={[styles.screenContainer, style]}>
+                            {children}
+                        </View>
+                    )}
+                </KeyboardAvoidingView>
+            </SafeAreaView>
             {!hideTabBar && (
                 <TabBar />
             )}
