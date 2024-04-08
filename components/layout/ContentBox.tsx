f1ebb088e5559357aaacda7a1d39edc733e8b76a
Hallvard Hetlelid <epichallvard@hotmail.com>
2024-04-08 22:57:35 +0200

    Created separate ContentBox for ribbon title

--- components/layout/ContentBox.tsx
+++ components/layout/ContentBox.tsx
@@ -8,7 +8,6 @@ import { componentColors } from "../misc/Colors";
 import Text from "../generalUI/Text";
 import Shadow from "../misc/Shadow";
 import { useContest } from "../../hooks/useContest";
-import RibbonTitle from "../generalUI/RibbonTitle";
 import { LinearGradient } from "expo-linear-gradient";
 
 interface ContentBoxProps {
@@ -23,14 +22,6 @@ interface ContentBoxProps {
     * @property Date used to determine the title for a contest title
     */
     date?: Date;
-    /** 
-    * @property Replaces the normal title with a big fancy title
-    */
-    ribbonTitle?: {
-        topText: string;
-        bottomText: string;
-        stars?: boolean;
-    }
     width?: DimensionValue;
     fetchEnabled?: boolean;
     containerStyle?: StyleProp<ViewStyle>;
@@ -50,7 +41,6 @@ export default function ContentBox(props: ContentBoxProps) {
         headerColor = componentColors.contentBox.highlight,
         isLoading = false,
         date,
-        ribbonTitle,
         width = "88%",
         fetchEnabled = true,
         containerStyle,
@@ -69,7 +59,7 @@ export default function ContentBox(props: ContentBoxProps) {
     const contest = useContest(date, fetchEnabled);
 
     return (
-        <View style={[styles.container, ribbonTitle ? { marginTop: 25 } : null, containerStyle]}>
+        <View style={[styles.container, containerStyle]}>
             <Shadow height={containerHeight} shadowHeight={rainbowBorder ? 4 : 8} width={width} borderRadius={20} />
             <View style={[
                 styles.background,
@@ -86,16 +76,11 @@ export default function ContentBox(props: ContentBoxProps) {
                 onLayout={onLayout}
             >
                 <View style={styles.contentBoxContainer}>
-                    {ribbonTitle && (
-                        <View style={styles.ribbonTitleConatiner}>
-                            <RibbonTitle stars={false} topText={ribbonTitle.topText} bottomText={ribbonTitle.bottomText ? ribbonTitle.bottomText : title} />
-                        </View>
-                    )}
                     {isLoading ? (
                         null // TODO: add loading indicator
                     ) : (
                         <>
-                            {!ribbonTitle && title && (
+                            {title && (
                                 <View style={[styles.titleContainer, { backgroundColor: headerColor }]}>
                                     <Text shadow={false}>{title ? title : contest.topic}</Text>
                                 </View>
@@ -105,7 +90,7 @@ export default function ContentBox(props: ContentBoxProps) {
                                     <Text shadow={false} color={textColor} style={{ textAlign: "center" }}>{text}</Text>
                                 </View>
                             )}
-                            <View style={[ribbonTitle ? { marginTop: 40 } : null, { gap: 10 }, { paddingTop: 10 }]}>
+                            <View style={{ gap: 10, paddingTop: 10 }}>
                                 {children}
                             </View>
                         </>
@@ -167,6 +152,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         minHeight: 40,
+        paddingTop: 10,
     },
 
     bottomContainer: {
@@ -175,10 +161,4 @@ const styles = StyleSheet.create({
         alignItems: "center",
         marginTop: 10,
     },
-
-    ribbonTitleConatiner: {
-        position: "absolute",
-        alignSelf: "center",
-        top: -30,
-    }
 })
