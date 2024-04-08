bd6981f9c618e475a241ab6f429422a6afd53fb2
Hallvard Hetlelid <epichallvard@hotmail.com>
2024-04-08 22:28:49 +0200

    Fixed UI issue with discount not showing correctly on iOS

--- components/misc/StoreListing.tsx
+++ components/misc/StoreListing.tsx
@@ -25,21 +25,23 @@ export default function StoreListing(props: StoreListingProps) {
     }
 
     return (
-        <ContentBox
-            title={title}
-            width={"100%"}
-            containerStyle={{ width: "42%" }}
-        >
-            <View style={styles.listingContainer}>
-                <Image style={{ height: 85, width: 85, marginTop: 6, }} source={images[icon]} />
-                <View style={styles.textContainer}>
-                    {oldPrice && (
-                        <Text size={15} style={{ textDecorationLine: "line-through", textDecorationStyle: 'solid' }} shadow={false} color={colors.metals.silver}>{oldPrice}</Text>
-                    )}
-                    <Text shadow={false} color={colors.purple.medium}>{price}</Text>
+        <View style={{ width: "42%" }}>
+            <ContentBox
+                title={title}
+                width={"100%"}
+                containerStyle={{ width: "100%" }}
+            >
+                <View style={styles.listingContainer}>
+                    <Image style={{ height: 85, width: 85, marginTop: 6, }} source={images[icon]} />
+                    <View style={styles.textContainer}>
+                        {oldPrice && (
+                            <Text size={15} style={{ textDecorationLine: "line-through", textDecorationStyle: 'solid' }} shadow={false} color={colors.metals.silver}>{oldPrice}</Text>
+                        )}
+                        <Text shadow={false} color={colors.purple.medium}>{price}</Text>
+                    </View>
+                    <Button height={36} label="Buy" variant="blue" />
                 </View>
-                <Button height={36} label="Buy" variant="blue" />
-            </View>
+            </ContentBox>
             {discount && (
                 <View style={styles.discountContainer}>
                     <PulseAnimation>
@@ -47,7 +49,7 @@ export default function StoreListing(props: StoreListingProps) {
                     </PulseAnimation>
                 </View>
             )}
-        </ContentBox>
+        </View>
     )
 }
 
@@ -68,8 +70,8 @@ const styles = StyleSheet.create({
 
     discountContainer: {
         position: "absolute",
-        top: -50,
-        right: -30,
+        // top: -50,
+        right: -16,
         transform: [{ rotate: '20deg' }],
         zIndex: 9999,
     }
